65902f1d3dce78a95008fda4ff666c99fea493ba
Chad Kellum <chad.kellum@tlgcohort.com>
2023-02-21 14:51:02 -0800

    Lab 9.1 complete

---
+++ Lab09.1/src/com/hr/personnel/SalariedEmployee.java
@@ -0,0 +1,52 @@
+package com.hr.personnel;
+
+import gov.irs.TaxPayer;
+
+import java.time.LocalDate;
+
+public class SalariedEmployee extends Employee {
+
+    private double salary;
+
+    // constructors
+    public SalariedEmployee() {
+        super();  // constructor chaining. This super is automatically put in the ctor whether it is visible or not.
+    }
+
+    public SalariedEmployee(String name, LocalDate hireDate) {
+        super(name, hireDate);
+    }
+    public SalariedEmployee(String name, LocalDate hireDate, double salary){
+        this(name, hireDate); // delegate to neighboring ctor for name, hireDate
+        setSalary(salary);    // handle salary myself, by delegating to setter
+    }
+    // business methods
+    @Override
+    public void pay() {
+        System.out.println(getName() + " is paid salary. Monthly pay is " + (getSalary() / 12) + " (before taxes!)" );
+    }
+
+    public void takeVacation() {
+        System.out.println(getName() + " is on vacation");
+    }
+
+    // accessor methods
+
+    public double getSalary() {
+        return salary;
+    }
+
+    public void setSalary(double salary) {
+        this.salary = salary;
+    }
+    @Override
+    public String toString() {
+        return super.toString() + ", salary=" + getSalary();
+    }
+
+    @Override
+    public void payTaxes() {
+        double taxes = salary * SALARIED_TAX_RATE;
+        System.out.println(getName() + " paid taxes " + taxes);
+    }
+}
--- Lab09.1/src/gov/irs/TaxPayer.java
+++ Lab09.1/src/gov/irs/TaxPayer.java
@@ -21,4 +21,5 @@ public interface TaxPayer {
     public static final double SALARIED_TAX_RATE = 0.30;
     
     public void payTaxes();
+
 }
